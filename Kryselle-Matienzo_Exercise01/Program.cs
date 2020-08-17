62c5cd29d6604675467966ff2db274f8654c950d
kcmatienzo <52175995+kcmatienzo@users.noreply.github.com>
2020-08-17 17:16:41 -0400

    Add project files.

---
+++ Kryselle-Matienzo_Exercise01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kryselle_Matienzo_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // using built-in delegate template
+            //Func<string, string, string, string> minString = Minimum;
+            //Console.WriteLine(minString("apple", "ant", "banana"));
+            //Console.WriteLine(minString("Twitter", "facebook", "Instagram"));
+
+            // CODE FROM NINA
+            Func<string, string, string, string> minimum = (str1, str2, str3) => 
+            {
+                string minStr = str1; // Asusme str1 is the smallest
+
+                // compare str2 with minStr
+                if (str2.CompareTo(minStr) < 0)
+                {
+                    minStr = str2; // str is the smallest for now
+                }
+
+                // compare to str3 with minStr
+                if (str3.CompareTo(minStr) < 0)
+                {
+                    minStr = str3;
+                }
+
+                return minStr;
+            };
+
+
+            // Built-in delegate Action. average will be used to hold the ref of a method which takes 3 inputs and no return value
+            Action<double, double, double> average = AvgGrade;
+
+            // test using average reference
+            average(3, 4, 5);
+            average(95, 90, 100);
+
+
+            // TEST nina's code
+            Console.WriteLine($"The smallest string is: {minimum("ant", "plant", "dog")}");
+        }
+
+        //// Method that returns the smallest value of string
+        //public static string Minimum(string strOne, string strTwo, string strThree)
+        //{
+        //    string[] words = { strOne, strTwo, strThree };
+        //    Array.Sort(words, StringComparer.Ordinal);
+        //    return words[0];
+        //}
+
+        // Method to display the average
+        private static void AvgGrade(double value1, double value2, double value3) => Console.WriteLine($"Average Grade is: " + (value1 + value2 + value3) / 3);
+    }
+}
---
+++ Kryselle-Matienzo_Exercise02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kryselle_Matienzo_Exercise02
+{
+    // defining a delegate that takes an input of type double and returns a bool
+    delegate bool GradesPredicate(double value);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring array of grades.
+            // I have 9 items which has value of 50 and up
+            double[] gradesArray = { 99.2, 36.7, 88.6, 67.9, 86.5, 17.6, 98.3, 46.34, 67.45, 26.53, 75.57, 89.56, 50.50 };
+
+            // Lambda expression that filters values which are greater or equal to 50.
+            GradesFilter(gradesArray, num => (num >= 50));
+        }
+
+        // Method that only displays those grades values which are greater than or equal to 50.
+        private static void GradesFilter(double[] gradesArray, GradesPredicate predicate)
+        {
+            foreach (var item in gradesArray)
+            {
+                // if the element satisfies the predicate
+                if (predicate(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            
+        }
+    }
+}
